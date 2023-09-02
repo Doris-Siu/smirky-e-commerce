fa3a08583f7899885c78d7baa640ad3139a70c5b
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-02 13:28:25 +0100

    add select dropdown functionality

---
+++ Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace TangyWeb_Server.Pages.LearnBlazor
+{
+	public class Demo_ProductProp
+	{
+		public int Id { get; set; }
+		public string Key { get; set; }
+		public string Value { get; set; }
+	}
+}
+
--- Tangy_Models/LearnBlazorModels/Demo_Products.cs
+++ Tangy_Models/LearnBlazorModels/Demo_Products.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using TangyWeb_Server.Pages.LearnBlazor;
+
 namespace Tangy_Models.LearnBlazorModels
 {
 	public class Demo_Products
@@ -7,6 +9,8 @@ namespace Tangy_Models.LearnBlazorModels
 		public string Name { get; set; }
 		public bool IsActive { get; set; }
 		public double Price { get; set; }
+
+		public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
 	}
 }
 
