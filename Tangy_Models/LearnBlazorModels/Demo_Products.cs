55b9324b79780271f0e8bf5c9591a36d5f03e024
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-02 12:09:16 +0100

    create a new razor component BindProp & perform two-way data binding

---
+++ Tangy_Models/LearnBlazorModels/Demo_Products.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Tangy_Models.LearnBlazorModels
+{
+	public class Demo_Products
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public bool IsActive { get; set; }
+		public double Price { get; set; }
+	}
+}
+
