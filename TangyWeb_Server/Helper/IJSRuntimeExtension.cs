128f527a0b6f4ee1bff62564a75bf9e2d4e63502
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-10 14:54:05 +0100

    create extension methods of IJSRuntime

---
+++ TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async ValueTask ToatrSuccess(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+
+        }
+
+        public static async ValueTask ToatrError(this IJSRuntime jsRuntime, string message)
+        {
+            await jsRuntime.InvokeVoidAsync("ShowToastr","error", message);
+
+        }
+    }
+}
+
