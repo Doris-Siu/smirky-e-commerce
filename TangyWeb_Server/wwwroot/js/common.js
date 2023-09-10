601d7a8555ddfc4b52d47691f7b35fa06aefb47a
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-10 16:08:32 +0100

    incorporate Swal & show alerts

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -8,3 +8,20 @@
 
     }
 }
+
+window.ShowSwal = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification!',
+            message,
+            'success'
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification!',
+            message,
+            'error'
+        )
+    }
+}
