94482f82745f4382d87d5cbca036a5c3360c3522
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-12 13:57:18 +0100

    set up ApplicationDbContext

---
+++ Tangy_DataAccess/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Tangy_DataAccess
+{
+	public class Category
+	{
+		public int Id{ get; set; }
+
+		public string Name { get; set; }
+
+		public DateTime CreatedDate { get; set; }
+
+	}
+}
+
---
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tangy_DataAccess.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+		{
+			
+		}
+
+
+		public DbSet<Category> Categories { get; set; }
+	}
+
+
+
+}
+
