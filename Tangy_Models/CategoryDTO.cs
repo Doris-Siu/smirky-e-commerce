3cba6dbf950ef6352d176591504fa12701fb2f0f
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-17 17:20:25 +0100

    add validation to form

--- Tangy_Models/CategoryDTO.cs
+++ Tangy_Models/CategoryDTO.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Tangy_Models
 {
 	public class CategoryDTO
 	{
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter name..")]
         public string Name { get; set; }
     }
 }
