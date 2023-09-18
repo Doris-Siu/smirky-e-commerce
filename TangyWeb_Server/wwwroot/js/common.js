e92cded6cee32a6303efc1ee89f6ee944c717cfe
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-18 16:48:45 +0100

    create Delete Confirmation modal component

--- TangyWeb_Server/wwwroot/js/common.js
+++ TangyWeb_Server/wwwroot/js/common.js
@@ -25,3 +25,13 @@ window.ShowSwal = (type, message) => {
         )
     }
 }
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+
+}
