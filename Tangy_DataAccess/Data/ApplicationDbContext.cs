b362fe1e06ecbb0986c99a91767e961529263136
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-21 15:18:30 +0100

    create Product table & add Db migration

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -11,7 +11,9 @@ namespace Tangy_DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
-	}
+        public DbSet<Product> Products { get; set; }
+
+    }
 
 
 
---
+++ Tangy_DataAccess/Migrations/20230921141314_added_product.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Tangy_DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_product : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Desciption = table.Column<string>(type: "text", nullable: false),
+                    ShopFavorites = table.Column<bool>(type: "boolean", nullable: false),
+                    CustomerFavorites = table.Column<bool>(type: "boolean", nullable: false),
+                    Color = table.Column<string>(type: "text", nullable: false),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_CategoryId",
+                table: "Products",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
