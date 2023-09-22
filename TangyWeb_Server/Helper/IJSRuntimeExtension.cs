697d5b0d3de7a6a234044708d2a28ef14ba8bd6e
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-23 00:19:29 +0100

    add file upload functionality & Handler function

--- TangyWeb_Server/Helper/IJSRuntimeExtension.cs
+++ TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -5,13 +5,13 @@ namespace TangyWeb_Server.Helper
 {
     public static class IJSRuntimeExtension
     {
-        public static async ValueTask ToatrSuccess(this IJSRuntime jsRuntime, string message)
+        public static async ValueTask ToastrErrorSuccess(this IJSRuntime jsRuntime, string message)
         {
             await jsRuntime.InvokeVoidAsync("ShowToastr", "success", message);
 
         }
 
-        public static async ValueTask ToatrError(this IJSRuntime jsRuntime, string message)
+        public static async ValueTask ToastrError(this IJSRuntime jsRuntime, string message)
         {
             await jsRuntime.InvokeVoidAsync("ShowToastr","error", message);
 
