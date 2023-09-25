852bd87015dcb67539d655659c5e56b6ae087de9
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-25 23:18:56 +0100

    return ProductPrices from Product API

--- Tangy_Models/ProductDTO.cs
+++ Tangy_Models/ProductDTO.cs
@@ -19,6 +19,8 @@ namespace Tangy_Models
         public int CategoryId { get; set; }
 
         public CategoryDTO Category { get; set; }
+
+        public ICollection<ProductPriceDTO> ProductPrices { get; set; }
     }
 }
 
