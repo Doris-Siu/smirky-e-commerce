0ec82bbed747f44edd49907561f5a2e46a14fe15
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-26 17:42:23 +0100

    implement ProductSerivce interface

---
+++ TangyWeb_Client/Service/IService/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tangy_Models;
+
+namespace TangyWeb_Client.Service.IService
+{
+	public interface IProductService
+	{
+        public Task<IEnumerable<ProductDTO>> GetAll();
+        public Task<ProductDTO> Get(int productId);
+    }
+}
+
---
+++ TangyWeb_Client/Service/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Service
+{
+	public class ProductService : IProductService
+	{
+		public ProductService()
+		{
+		}
+
+        public Task<ProductDTO> Get(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<ProductDTO>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
