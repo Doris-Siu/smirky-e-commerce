b6d695c3ac5374fe65ba3575f5cb11e6fa9b3570
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-28 16:56:12 +0100

    implement create operation of Order

--- Tangy_DataAccess/Data/ApplicationDbContext.cs
+++ Tangy_DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,9 @@ namespace Tangy_DataAccess.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductPrice> ProductPrices { get; set; }
 
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
 
     }
 
