e00af5d3c1b772a68970ea05b8ed3a648fa65255
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-30 20:32:08 +0100

    config auth & authorization

--- TangyWeb_Client/Program.cs
+++ TangyWeb_Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using TangyWeb_Client;
@@ -16,6 +17,9 @@ builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
 
 builder.Services.AddBlazoredLocalStorage();
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthStateProvider>();
 
 await builder.Build().RunAsync();
 
