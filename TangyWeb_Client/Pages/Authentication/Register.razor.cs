9047aff4403a4a4a00e95e28d50cd9cd2e6e6b46
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-30 23:05:06 +0100

    split code file for Register component

---
+++ TangyWeb_Client/Pages/Authentication/Register.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class Register
+    {
+        private SignUpRequestDTO SignUpRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowRegistrationErrors { get; set; }
+        public IEnumerable<string> Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task RegisterUser()
+        {
+            ShowRegistrationErrors = false;
+            IsProcessing = true;
+            var result = await _authService.RegisterUser(SignUpRequest);
+            if (result.IsRegisterationSuccessful)
+            {
+                //regiration is successful
+                _navigationManager.NavigateTo("/login");
+            }
+            else
+            {
+                //failure
+                Errors = result.Errors;
+                ShowRegistrationErrors = true;
+
+            }
+            IsProcessing = false;
+        }
+    }
+}
+
