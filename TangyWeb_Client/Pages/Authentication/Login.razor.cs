312f15e760843082303a9bb64bb370e409fcb0de
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-30 23:45:55 +0100

    refine Login component & add logic

---
+++ TangyWeb_Client/Pages/Authentication/Login.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Tangy_Models;
+using TangyWeb_Client.Service.IService;
+
+namespace TangyWeb_Client.Pages.Authentication
+{
+    public partial class Login
+    {
+        private SignInRequestDTO SignInRequest = new();
+        public bool IsProcessing { get; set; } = false;
+        public bool ShowSignInErrors { get; set; }
+        public string Errors { get; set; }
+
+        [Inject]
+        public IAuthenticationService _authService { get; set; }
+        [Inject]
+        public NavigationManager _navigationManager { get; set; }
+
+        private async Task LoginUser()
+        {
+            ShowSignInErrors = false;
+            IsProcessing = true;
+            var result = await _authService.Login(SignInRequest);
+            if (result.IsAuthSuccessful)
+            {
+                //sign in is successful
+                _navigationManager.NavigateTo("/");
+            }
+            else
+            {
+                //failure
+                Errors = result.ErrorMessage;
+                ShowSignInErrors = true;
+
+            }
+            IsProcessing = false;
+        }
+    }
+}
+
