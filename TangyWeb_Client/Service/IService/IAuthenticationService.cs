42820511f3b63b9ab030994a8903ace239466300
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-30 20:37:03 +0100

    create  IAuthenticationSerivce

---
+++ TangyWeb_Client/Service/IService/IAuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Tangy_Models;
+
+namespace TangyWeb_Client.Service.IService
+{
+	public interface IAuthenticationService
+	{
+
+        Task<SignUpResponseDTO> RegisterUser(SignUpRequestDTO signUpRequestDTO);
+        Task<SignInResponseDTO> Login(SignInRequestDTO signInRequestDTO);
+        Task Logout();
+    }
+}
+
