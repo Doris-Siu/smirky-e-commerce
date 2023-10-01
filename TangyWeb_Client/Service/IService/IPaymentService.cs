a4f0116e77379f3d9fcbdca439db418cabb37a0a
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-10-01 23:19:40 +0100

    add PaymentService in WASM

---
+++ TangyWeb_Client/Service/IService/IPaymentService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Tangy_Models;
+
+namespace TangyWeb_Client.Service.IService
+{
+	public interface IPaymentService
+	{
+        public Task<SuccessModelDTO> Checkout(StripePaymentDTO model);
+    }
+}
+
