762455ca5e41f339b547781ecc77405a4d46260c
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-10-03 17:06:17 +0100

    add PaymentIntentId

--- TangyWeb_API/Controllers/StripePaymentController.cs
+++ TangyWeb_API/Controllers/StripePaymentController.cs
@@ -65,7 +65,7 @@ namespace TangyWeb_API.Controllers
                 Session session = service.Create(options);
                 return Ok(new SuccessModelDTO()
                 {
-                    Data = session.Id
+                    Data = session.Id + ";" + session.PaymentIntentId
                 });
             }
             catch (Exception ex)
